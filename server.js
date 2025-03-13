5468ac69137b3c1d46c00011f57ebd6e831eba42
Aafhan Javed <aafhanjaved@gmail.com>
2025-03-13 07:23:35 +0500

    converted to env

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const mqtt = require("mqtt");
 const admin = require("firebase-admin");
+// Convert private key (Handle newlines properly)
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: privateKey,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 // Initialize Firebase
-const serviceAccount = require("./firebase-key.json");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://shaka-firebase-default-rtdb.firebaseio.com/", // Replace with your database URL
